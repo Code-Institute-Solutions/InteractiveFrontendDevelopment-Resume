fc8e28de9b6c8ef38f64cfb4b6323e7cdbfed587
berat.zorlu <berat.zorlu@hotmail.com>
2023-02-26 15:49:37 +0000

    Add GitHub user repo info display logic.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -14,6 +14,27 @@ function userInformationHTML(user) {
             </div>`;
 }
 
+function repoInformationHTML (repos) {
+    if (repos.length == 0) {
+        return `<div class='clearfix repo-list'>User has no repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                   <a href='${repo.html_url}' target='_blank'>${repo.name}</a>     
+                </li>`
+    });
+
+    return `<div class='clearfix repo-list'>
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join('/n')}
+                </ul>
+            </div>`;
+
+}
 
 function fetchGitHubInformation(event) {
     var username = $('#gh-username').val();
