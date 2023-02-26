c0786602b33de7b59c4cc24034d46cc19536ce62
berat.zorlu <berat.zorlu@hotmail.com>
2023-02-26 15:52:30 +0000

    Fix empty input bug.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,9 @@ function repoInformationHTML (repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $('#gh-user-data').html('');
+    $('#gh-repo-data').html('');
+
     var username = $('#gh-username').val();
     if (!username) {
         $('#gh-user-data').html(`<h2>Please enter a GitHub username</h2>`);
