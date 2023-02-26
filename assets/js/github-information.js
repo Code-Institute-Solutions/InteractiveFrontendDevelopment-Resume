2f5eb489af3cde21a8fb7dd0345e36ee1b5f9a14
berat.zorlu <berat.zorlu@hotmail.com>
2023-02-26 15:22:02 +0000

    Add GitHub username display feature.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,20 @@
+function userInformationHTML(user) {
+    return `<h2>${user.name}
+                <span class='small-name'>
+                    (@<a href='${user.html_url}' target='_blank'>${user.login}</a>)
+                </span>
+            </h2>
+            <div class='gh-content'>
+                <div class='gh-avatar'>
+                    <a href='${user.html_url}' target='_blank'>
+                        <img src='${user.avatar_url}' width='80' height='80' alt='${user.login}'>
+                    </a>
+                </div>
+                <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+            </div>`;
+}
+
+
 function fetchGitHubInformation(event) {
     var username = $('#gh-username').val();
     if (!username) {
