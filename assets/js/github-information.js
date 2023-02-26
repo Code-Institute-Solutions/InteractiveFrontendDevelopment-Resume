78ea067ea53dae6cb89436551cf69452d53ee465
berat.zorlu <berat.zorlu@hotmail.com>
2023-02-26 15:05:04 +0000

    Add error response for invalid user input.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -9,4 +9,18 @@ function fetchGitHubInformation(event) {
         `<div id='loader'>
             <img src='assets/css/loader.gif' alt='loading...'>
         </div>`);
+
+    $.when($.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $('#gh-user-data').html(userInformationHTML(userData));
+        }, function (errorResponse) {
+            if (errorResponse.status == 404) {
+                $('#gh-user-data').html(`<h2>No info found for user ${username}...</h2>`);
+            } else {
+                console.log(errorResponse);
+                $('#gh-user-data').html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
