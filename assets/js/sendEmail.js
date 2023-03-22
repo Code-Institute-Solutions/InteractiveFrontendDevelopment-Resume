56e01596230edb8444231e9cf5b830ae0af83857
berat.zorlu <berat.zorlu@hotmail.com>
2023-03-22 23:00:57 +0000

    Add automated email functionality.

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false
+};
